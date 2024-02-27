7afb7b3f2248bab5946899b7cb380a76d2f901d1
Kamil <108445618+crawford-kamil@users.noreply.github.com>
2024-02-27 14:33:05 -0500

    Complete first test of exercise four remove from array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...omissions) {
+    for (let i = 0; i < omissions.length; i++) {
+        let omissionsIndex = array.indexOf(omissions[i]);
+        array.splice(omissionsIndex, 1)
+    }
+    return array;
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
+/*
+    let omissionsIndex = array.indexOf(omissions);
+    array.splice(omissionsIndex, 1);
+    return array;
+*/
